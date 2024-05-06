6a7abbc692ddfcaa9603f80501a66adcd6bd6f9e
Everesh <everesh@proton.me>
2024-05-06 23:36:50 +0200

    Add print method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,20 @@
 class TicTacToe
+
+  def attr_reader :board
+
   def initialize
-    @board = Array.new(3) { Array.new(3, nil) }
+    @board = Array.new(3) { Array.new(3, " ") }
   end
+
+  def print_state
+    puts "+---+---+---+"
+    puts "| #{board[0][0]} | #{board[0][1]} | #{board[0][2]} |"
+    puts "+---+---+---+"
+    puts "| #{board[1][0]} | #{board[1][1]} | #{board[1][2]} |"
+    puts "+---+---+---+"
+    puts "| #{board[2][0]} | #{board[2][1]} | #{board[2][2]} |"
+    puts "+---+---+---+"
+  end
+
 end
 
