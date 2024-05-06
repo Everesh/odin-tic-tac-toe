7ceddaad8f98ac62d7bc32ee4e60cf22ceeaf8d7
Everesh <everesh@proton.me>
2024-05-06 23:54:47 +0200

    Add some sass to init

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,15 +4,22 @@ class TicTacToe
 
   def initialize
     @board = Array.new(3) { Array.new(3, " ") }
+    puts "\nTwo challengers dare to approach the colloseum of Tic of the Toe? The Tac?"
+    puts "Very well. Let the duel begin!"
+    print_state
   end
 
+  private
+
   def print_state
-    acc = ""
+    acc = "\n"
     board.each do | row |
         acc += "+---+---+---+\n"
         acc += "| #{row[0]} | #{row[1]} | #{row[2]} |\n"
     end
-    puts acc + "+---+---+---+"
+    puts acc + "+---+---+---+\n"
   end
 
 end
+
+game = TicTacToe.new #delete this after
