c89a1635789df49654df10aaf6e306cf15bf7dec
Everesh <everesh@proton.me>
2024-06-01 01:07:06 +0200

    Implement row test

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,4 +1,16 @@
 require_relative '../lib/tic_tac_toe'
 
 describe TicTacToe do
+  subject(:game) { described_class.new }
+
+  describe '#winner?' do
+    context 'when 3 in row' do
+      it 'returns true' do
+        allow(game).to receive(:board).and_return([[' ', ' ', 'O'],
+                                                   ['X', 'X', 'X'],
+                                                   ['O', ' ', ' ']])
+        expect(game.winner?).to be true
+      end
+    end
+  end
 end
