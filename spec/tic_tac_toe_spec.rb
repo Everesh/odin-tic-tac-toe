e7daf9827549a9e3415566f2dc76274abc23de4f
Everesh <everesh@proton.me>
2024-06-01 01:08:30 +0200

    Expand row tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -5,10 +5,22 @@ describe TicTacToe do
 
   describe '#winner?' do
     context 'when 3 in row' do
-      it 'returns true' do
+      it 'first row returns true' do
+        allow(game).to receive(:board).and_return([['O', 'O', 'O'],
+                                                   ['X', 'X', ' '],
+                                                   ['O', 'X', 'X']])
+        expect(game.winner?).to be true
+      end
+      it 'second row returns true' do
         allow(game).to receive(:board).and_return([[' ', ' ', 'O'],
                                                    ['X', 'X', 'X'],
                                                    ['O', ' ', ' ']])
+        expect(game.winner?).to be true
+      end
+      it 'third row returns true' do
+        allow(game).to receive(:board).and_return([['X', 'X', 'O'],
+                                                   ['X', ' ', 'X'],
+                                                   ['O', 'O', 'O']])
         expect(game.winner?).to be true
       end
     end
